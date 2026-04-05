f5893189e2aa3fe4c6bc83cd676c9076729a9069
Tgn <114723993+tgnlex@users.noreply.github.com>
2026-04-05 10:08:32 -0400

    Update highlight.lua

--- lua/config/autocommands/highlight.lua
+++ lua/config/autocommands/highlight.lua
@@ -1,4 +1,4 @@
 vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function() vim.hl.on_yank() end,
+  callback = function() vim.highlight.on_yank() end,
   desc = "Briefly highlight yanked text"
 })
